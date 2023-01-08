90bc69f969fc91a4317f41e92a18f38e0571b4cb
JanFidor <jan.p.fidor@gmail.com>
2023-01-08 17:14:13 +0100

    set up navigation

--- app/src/main/java/com/example/pappokedex/MainActivity.kt
+++ app/src/main/java/com/example/pappokedex/MainActivity.kt
@@ -2,13 +2,28 @@ package com.example.pappokedex
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setupActionBarWithNavController(this, navController)
+
+        val bottomNavigationView =
+            findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
+
+
 }
